999454cc31dbcc2c8017923113725638b1e03179
wnagdidi <80252973+wnagdidi@users.noreply.github.com>
2026-02-26 11:01:39 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,26 @@
+#!/bin/bash
 set -ex
 
+# 拉取最新代码 
 git pull
+# 安装依赖
 pnpm i
 
-export NODE_ENV=production
+# 配置区（可以修改）
+APP_NAME="avoid-ai-web"
+PORT="8008"
 
+# 行为1：start
+ACTION=$1
+
+# 构建
+export NODE_ENV=production
 pnpm build
 
-# regular
-pm2 restart avoidai-web
+# 停用旧进程（忽略错误）
+pm2 del ${APP_NAME}
 
-# first deploy
-#pm2 del avoidai-web
-#pm2 start "NODE_ENV=production pnpm start" --name "avoidai-web"
+# 启动新进程
+pm2 ${ACTION} "NODE_ENV=production PORT='${PORT}' pnpm ${ACTION}" --name ${APP_NAME}
+echo "=== 部署完成 ==="
+pm2 list | grep ${APP_NAME}
